879f4387e937b1bf21c17705597d9055ac88876b
cheng_oragne <2680618660@qq.com>
2024-04-07 23:27:15 +0800

    新增初始化数据界面及可以使用id查找精灵

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import {init} from './utils/init'
 import Loading from './components/Loading/Loading';
 import  {PROGRESS_MAP} from './interface/iData';
+import {Pet} from './page/Pet';
 
 function App() {
   const [data,setData]=useState(new Map());
@@ -10,7 +11,7 @@ function App() {
   const [progress, setProgress] = useState({item:'',index:0});
   return (
     <div>
-      {progress.index!=17? <Loading width={progress.index*20} item={PROGRESS_MAP.get(progress.item) as string}></Loading> : <div>欢迎使用Seer</div>}
+      {progress.index!=17? <Loading width={progress.index*20} item={PROGRESS_MAP.get(progress.item) as string}></Loading> : <Pet pets={data.get('monsters')}></Pet>}
       <p style={{margin:0,fontSize: '10px',position:'absolute',right:0,bottom:0}}>版本号：{data.get('version')}</p>
     </div>
   )
---
+++ src/page/Pet.tsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import iMonster from '../interface/iMonster'
+
+export const Pet = ({pets}:{pets:iMonster}) => {
+	//console.log(pets);
+	const [id,setId] = useState(300);
+	return (
+		<>
+		<input type="number" onChange={(e) => setId(parseInt(e.target.value))}/>
+		<div>{id>=1?pets.Monsters.Monster.find(i=>i.ID===id)?.DefName:"请输入正确的id"}</div>
+		{id?<img src={`https://seerh5.61.com/resource/assets/pet/head/${id}.png`}></img>:null}
+		
+		</>
+	)
+}
